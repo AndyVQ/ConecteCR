5a2e07c49db65124c36134fe3dfae9b572c33ecc
AndyVQ <quesadaandy0@gmail.com>
2025-06-05 11:46:22 -0600

    avance Andy

--- FE/conecta_front/src/pages/PagHome.jsx
+++ FE/conecta_front/src/pages/PagHome.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import Navbar from "../components/NavBar";
 import CarouselComp from "../components/CarouselComp";
-import Cards from "../components/Card"; 
-import FooterHighFashion from "../components/Footer"; 
-
+import Cards from "../components/Card";
+import FooterHighFashion from "../components/Footer";
+import CardNoticia from "../components/CardNoticia";
 
 const PagHome = () => {
   return (
@@ -11,23 +11,28 @@ const PagHome = () => {
       <header>
         <Navbar />
       </header>
-      
+
       <article>
         <CarouselComp />
       </article>
-      
+      <div>
+        <CardNoticia
+          title="Noticia 3"
+          description="Aquí va la descripción de la noticia 3. Breve resumen."
+          imageUrl="https://via.placeholder.com/350x200"
+          link="https://www.ejemplo.com/noticia3"
+        />
+      </div>
+
       <div className="pag-Home">
         <Cards />
       </div>
+
       <footer>
-        <FooterHighFashion/>   
+        <FooterHighFashion />
       </footer>
-    
     </>
-    
   );
 };
 
 export default PagHome;
-
-
