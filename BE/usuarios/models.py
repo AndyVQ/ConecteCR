3a8def3ea7d687032cc8a6e1d61382f4a40946f4
MarioCoronado <mariocoronado1000@gmail.com>
2025-06-19 15:43:58 -0600

    Avance

--- BE/usuarios/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Perfil(models.Model):
-   usuario = models.OneToOneField(User,on_delete=models.CASCADE)
-   cedula_usuario = models.CharField(max_length=100, unique=True, null=False, blank=False)
-   telefono_usuario = models.CharField(max_length=100)
-   image_usuario=models.CharField(max_length=100, null=True, blank=True)
-
